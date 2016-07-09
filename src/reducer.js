8bdc222705571fe67c824a15f4fab8a3ddbec250
Dan Miller <danmiller@bluedrop.com>
2016-07-09 21:01:21 -0230

    Clarifies code in reducer callbacks.

--- src/reducer.js
+++ src/reducer.js
@@ -5,12 +5,12 @@ const init = List([]);
 
 export default function(todos=init, action) {
 	return _.cond([
-		[_.matches({type: 'ADD_TODO'}), function (action) {
-			return todos.push(Map(action.payload));
+		[_.matches({type: 'ADD_TODO'}), function (addAction) {
+			return todos.push(Map(addAction.payload));
 		}],
-		[_.matches({type: 'TOGGLE_TODO'}), function () {
+		[_.matches({type: 'TOGGLE_TODO'}), function (toggleAction) {
 			return todos.map(t => {
-				return t.get('id') === action.payload
+				return t.get('id') === toggleAction.payload
 					? t.update('isDone', isDone => !isDone)
 					: t
 				;
