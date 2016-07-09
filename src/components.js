86423d69b5b6d35e965751d6eea94fe539015bb4
Dan Miller <danmiller@bluedrop.com>
2016-07-09 13:56:16 -0230

    Components calling actions via event listeners.

--- src/components.js
+++ src/components.js
@@ -9,14 +9,35 @@ export function Todo(props) {
 };
 
 export function TodoList(props) {
-	const { todos } = props;
+	const { todos, addTodo, toggleTodo } = props;
+	
+	const onSubmit = (event) => {
+		const input = event.target;
+		const text = input.value;
+		const isEnterKey = (event.which == 13);
+		const isLongEnough = text.length > 0;
+		
+		if (isEnterKey && isLongEnough) {
+			input.value = '';
+			addTodo(text);
+		}
+	};
+	
+	const toggleClick = id => event => toggleTodo(id);
+	
 	return (
 		<div className='todo'>
-			<input type="text" placeholder="Add Todo" />
+			<input
+				type="text"
+				className = "todo__entry"
+				placeholder="Add Todo"
+				onKeyDown={onSubmit} />
 			<ul className="todo__list">
 				{todos.map(t => (
-					<li key={t.get('id')} className='to__item'>
-						<Todo todo={t} />
+					<li key={t.get('id')}
+						className='to__item'
+						onClick={toggleClick(t.get('id'))}>
+						<Todo todo={t.toJS()} />
 					</li>
 				))}
 			</ul>
