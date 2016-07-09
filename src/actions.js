1c18e11f66af655d04c6b552b1d7fbab77d4ad34
Dan Miller <danmiller@bluedrop.com>
2016-07-09 13:22:51 -0230

    Adds actions.js

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+
+export function addTodo(text) {
+	return {
+		type: 'ADD_TODO',
+		payload: {
+			id: _.uniqueId(),
+			isDone: false,
+			text: test
+		}
+	}
+}
+
+export function toggleTodo (id) {
+	return {
+		type: 'TOGGLE_TODO',
+		payload: id
+	}
+}
