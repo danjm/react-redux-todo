9b11a071edf9600e52485c48e5a321615ca2ce5b
Dan Miller <danmiller@bluedrop.com>
2016-07-09 13:58:49 -0230

    Connecting containers to store via react-redux Provider.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { TodoList } from './components';
+import { Provider } from 'react-redux';
+import { TodoList } from './containers';
 import reducer from './reducer';
 
 const store = createStore(reducer);
 
 render(
-	<TodoList todos={store.getState()} />,
+	<Provider store={store}>
+		<TodoList />
+	</Provider,
 	document.getElementById('app')
 );
