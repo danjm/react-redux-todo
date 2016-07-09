5abae677eee5f380fb4d715f1667725fe3f7969c
Dan Miller <danmiller@bluedrop.com>
2016-07-08 23:46:14 -0230

    Fix npm and webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,12 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel',
-			query: {presets: ['es2015', 'react']}
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {presets: ['es2015', 'react']}
+			}
 		]
 	}
 };
